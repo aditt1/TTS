d5dd464027a52a15832b206b3c5993da8898a351
aditt1 <aditya.halimawan@si.ukdw.ac.id>
2019-10-28 11:16:22 +0700

    tugas TTS

--- app/src/main/java/com/example/tugastts/AdminActivity.java
+++ app/src/main/java/com/example/tugastts/AdminActivity.java
@@ -2,7 +2,10 @@ package com.example.tugastts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+
+        ImageButton dosenBtn = findViewById(R.id.btnDosen);
+        ImageButton dataBtn = findViewById(R.id.btnData);
+        ImageButton matkulBtn = findViewById(R.id.btnMatkul);
+        ImageButton krsBtn = findViewById(R.id.btnKRS);
+        ImageButton mhsBtn = findViewById(R.id.btnMhs);
+
+        dosenBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(AdminActivity.this,ListDosenActivity.class);
+                startActivity(i);
+            }
+        });
+
+        dataBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(AdminActivity.this,DataDosenActivity.class);
+                startActivity(i);
+            }
+        });
+
+        matkulBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(AdminActivity.this,ListMatkulActivity.class);
+                startActivity(i);
+            }
+        });
+
+        krsBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(AdminActivity.this,ListKRSActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mhsBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(AdminActivity.this,ListMahasiswaActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tugastts/DosenActivity.java
+++ app/src/main/java/com/example/tugastts/DosenActivity.java
@@ -2,7 +2,10 @@ package com.example.tugastts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DosenActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class DosenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dosen);
+
+        ImageButton krsBtn = findViewById(R.id.btnDaftarKrs);
+        ImageButton dataDiriBtn = findViewById(R.id.btnData);
+        ImageButton dataKelasBtn = findViewById(R.id.btnLihat);
+
+        krsBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(DosenActivity.this,ListKRSActivity.class);
+                startActivity(i);
+            }
+        });
+
+        dataDiriBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(DosenActivity.this,DataDosenActivity.class);
+                startActivity(i);
+            }
+        });
+
+        dataKelasBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(DosenActivity.this,LihatKelasActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
